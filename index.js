e74a26d7aa340116c85de65c013c81b37eb5f2d6
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-02-19 15:07:48 +0530

    m

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ const model = genAI.getGenerativeModel({
 
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: "500mb" }));
 app.use(cookieParser());
 
 app.use(session({
