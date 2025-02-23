9d12d40cef6876568e5f268ba964dbb96b303e8e
SyedZainulAbideen494 <zainkaleem27@gmail.com>
2025-02-24 00:19:07 +0530

    m

--- index.js
+++ index.js
@@ -10964,7 +10964,7 @@ app.get("/api/mindmaps/count/for-premium", async (req, res) => {
   }
 });
 
-app.get("/api/mindmaps/all", async (req, res) => {
+app.get("/api/mindmaps/all/user", async (req, res) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "No token provided" });
 
